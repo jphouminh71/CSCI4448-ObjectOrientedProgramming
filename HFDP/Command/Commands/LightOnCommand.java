3a7206c5cf59b53690979f9db5c4d1945e7963a9
JonathanPhouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2021-05-17 17:32:26 -0600

    added comments to command pattern file

--- HFDP/Command/Commands/LightOnCommand.java
+++ HFDP/Command/Commands/LightOnCommand.java
@@ -12,6 +12,11 @@ public class LightOnCommand implements Command {
     }
 
     // The invoker doesn't need to know how to execute the command, they just know that it can be execute
+    // If you wanted to capture the specific button that was pressed to execute a command accordingly, you could probably pass in the id of the button 
+    // in the paramters so you can execute accordingly, but you would have to map the commands to a specific button and interchanging commands might be annoying
+    // but in the case of a remote you don't normally remap buttons once they are set. 
+
+    // OR you can add a second 'execute' in the interface to support another execute method, then set all second executes to return null if they aren't being used. 
     public void execute() {
         this.light.flip(); 
     }    
